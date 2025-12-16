45f830a28b1621d30ccddf9347ca5aa43f644afd
aalipak <111370811+aalipak@users.noreply.github.com>
2025-12-16 20:28:58 +0500

    added map

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -275,12 +275,17 @@ const Contact = () => {
             viewport={{ once: true }}
           >
             <h2>Find Us</h2>
-            <div className="map-placeholder">
-              <div className="map-content">
-                <FaMapMarkerAlt className="map-icon" />
-                <p>Interactive map will be displayed here</p>
-                <p>Lahore, Pakistan</p>
-              </div>
+            <div className="map-wrapper">
+              <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3401.5231087350407!2d74.30699!3d31.5204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3919a90d5a89c26f%3A0x6f0b0b0b0b0b0b0b!2sLahore%2C%20Punjab%2C%20Pakistan!5e0!3m2!1sen!2s!4v1670000000000"
+                width="100%" 
+                height="450" 
+                style={{ border: 0, borderRadius: '12px' }}
+                allowFullScreen="" 
+                loading="lazy" 
+                referrerPolicy="no-referrer-when-downgrade"
+                title="George Packers N Movers Location - Lahore, Pakistan"
+              ></iframe>
             </div>
           </motion.div>
         </div>
